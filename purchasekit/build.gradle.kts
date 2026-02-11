ceb457848a12893fa6fdb354ef6df765843f6aec
Joe Masilotti <joe@masilotti.com>
2026-02-11 09:25:21 -0800

    Bump version to 0.4.5

--- purchasekit/build.gradle.kts
+++ purchasekit/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "dev.purchasekit"
             artifactId = "purchasekit"
-            version = "0.4.4"
+            version = "0.4.5"
 
             afterEvaluate {
                 from(components["release"])
